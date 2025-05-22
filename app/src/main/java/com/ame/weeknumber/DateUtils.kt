3f85e101dc39d8f8afa0a3fadc3ebadb57bccda4
Anton Melchakov <anton.melchakov@al-enterprise.com>
2025-05-22 16:13:53 +0300

    upgrade gradle

--- app/src/main/java/com/ame/weeknumber/DateUtils.kt
+++ app/src/main/java/com/ame/weeknumber/DateUtils.kt
@@ -5,7 +5,6 @@ import java.time.temporal.WeekFields
 import java.util.Locale
 
 fun getCurrentWeek(localDate: LocalDate = LocalDate.now()): Int {
-    val date = localDate
     val weekFields = WeekFields.of(Locale.FRANCE)
-    return date[weekFields.weekOfWeekBasedYear()]
+    return localDate[weekFields.weekOfWeekBasedYear()]
 }
